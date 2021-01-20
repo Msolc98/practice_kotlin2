cea046f7a6d78c4cc3fb7683faeda04edea52c30
Msolc98 <lillit0728@gmail.com>
2021-01-20 22:44:34 +0900

    2021.01.20.third

--- src/평균구하기2021.01.20.kt
+++ src/평균구하기2021.01.20.kt
@@ -1,21 +1,18 @@
 fun main() {
     var test = Solution()
-    test.solution(intArrayOf(1,2,3))
+    println(test.solution(intArrayOf(1,2,3)))
 }
 
 
 class Solution{
 
     fun solution(arr:IntArray):Double{
-        var sum = 0
+        var sum:Double = 0.0
         var answer:Double = 0.0
         arr.forEach {
                 i -> sum += i
-            println(arr[arr.size-1])
                 if (i == arr[arr.size-1]) {
-                    answer /= arr.size
-                    println(sum)
-                    println(answer)
+                    answer = sum / arr.size
                 }
         }
         return answer
