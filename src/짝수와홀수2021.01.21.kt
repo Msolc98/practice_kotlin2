cac5253d4670da49f96e43552520835ef5d2b540
Msolc98 <lillit0728@gmail.com>
2021-01-21 20:42:59 +0900

    2021.01.21.second

--- src/짝수와홀수2021.01.21.kt
+++ src/짝수와홀수2021.01.21.kt
@@ -1,12 +1,21 @@
 fun main() {
-    var test = Solution()
+    var test = Solution21first()
+    println(test.solution(4))
 }
 
-class Solution{
+class Solution21first{
     fun solution(num: Int): String {
         var answer = "Odd"
         if (num%2==0)
             answer = "Even"
         return answer
     }
+
+    fun answer(num: Int): String
+    {
+        return if (num.and(1) == 0) "Even" else "Odd"
+        //if (num and 1 == 1)
+    }
+
+    fun answer2(num: Int): String = if (num % 2 == 0) "Even" else "Odd"
 }
