3c82f34b42411930546093b41fc7fc1e32d30450
Msolc98 <lillit0728@gmail.com>
2021-01-25 21:58:29 +0900

    2021.01.25.third

--- src/제일작은수제거하기2021.01.25.kt
+++ src/제일작은수제거하기2021.01.25.kt
@@ -1,18 +1,23 @@
 fun main() {
-    var test = tmp()
-    println(test.test())
+    var test = Solution25first()
+    var tmp = intArrayOf(1,2,3)
+    //println(tmp.minOrNull())
+
 }
 
 class Solution25first{
 
-    fun solution(arr: IntArray): IntArray {
+    fun solution(arr: IntArray): IntArray { //정석으로 해보기
 
         var answer = intArrayOf(-1)
 
         if (arr.size > 1){
             var min = arr[0]
+            var min_index = 0
             arr.forEachIndexed { index, i ->
                 min = Math.min(min, i)
+                if (min == i)    // 변경o
+                    min_index = index
             }
 
         }
@@ -20,7 +25,8 @@ class Solution25first{
         return answer
     }
 
-    fun solution2(arr: IntArray): IntArray {
+
+    fun solution2(arr: IntArray): IntArray {    //음
 
         arr.sort()
         var answer =arr
@@ -30,4 +36,17 @@ class Solution25first{
 
         return answer
     }
+    
+    
+
+    fun solution3(arr: IntArray): IntArray { // 읽기전용 iterator 사용해보기
+
+        var iterator = arr.iterator()
+        
+        while(iterator.hasNext())
+            print("hi")
+
+        return arr
+    }
+
 }
