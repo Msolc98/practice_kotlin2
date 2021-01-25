b56d1e76d9300b552fac3031d807bd9a057d3735
Msolc98 <lillit0728@gmail.com>
2021-01-25 20:28:12 +0900

    2021.01.25.second

--- src/제일작은수제거하기2021.01.25.kt
+++ src/제일작은수제거하기2021.01.25.kt
@@ -4,18 +4,29 @@ fun main() {
 }
 
 class Solution25first{
+
     fun solution(arr: IntArray): IntArray {
 
-        var min = arr[0]
-        arr.forEachIndexed { index, i ->
-            if(i < min)
-                min = index
-            if(index == arr.lastIndex){
+        var answer = intArrayOf(-1)
 
+        if (arr.size > 1){
+            var min = arr[0]
+            arr.forEachIndexed { index, i ->
+                min = Math.min(min, i)
             }
+
         }
 
-        var answer = arr
+        return answer
+    }
+
+    fun solution2(arr: IntArray): IntArray {
+
+        arr.sort()
+        var answer =arr
+        for (i in 0..arr.lastIndex)
+            if(i+1 <= arr.lastIndex)
+                arr[i] = arr[i + 1]
 
         return answer
     }
