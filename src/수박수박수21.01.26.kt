77e969e68681665b04c29a34b219ea8c75bc0a39
Msolc98 <lillit0728@gmail.com>
2021-01-26 21:52:16 +0900

    2021.01.25.first

---
+++ src/수박수박수21.01.26.kt
@@ -0,0 +1,31 @@
+fun main() {
+    var test = solution26first()
+    println("\n" + test.solution(5))
+
+}
+class solution26first{
+    fun solution(n: Int) {
+        if(n%2 == 0)
+            for (i in 1..n/2)
+                print("수박")
+        else {
+            for (i in 1..n/2)
+                print("수박")
+            print("수")
+        }
+    }
+
+    fun solution2(n: Int):String {
+        var answer = ""
+        if(n%2 == 0)
+            for (i in 1..n/2)
+                answer += "수박"
+
+        else {
+            for (i in 1..n/2)
+                answer += "수박"
+            answer += "수"
+        }
+        return answer
+    }
+}
