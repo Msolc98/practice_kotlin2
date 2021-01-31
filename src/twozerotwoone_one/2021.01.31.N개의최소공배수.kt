826f517d3d9a6ed4d942f7d0c9ba52d89fb24933
Msolc98 <lillit0728@gmail.com>
2021-01-31 23:52:03 +0900

    2021.01.31.second

--- src/twozerotwoone_one/2021.01.31.N개의최소공배수.kt
+++ src/twozerotwoone_one/2021.01.31.N개의최소공배수.kt
@@ -7,6 +7,11 @@ fun main() {
 class solution31first{
     fun solution(arr: IntArray): Int {
         var answer = 0
+
+        if(arr.size==1)
+            return arr[0]
+        else
+
         return answer
     }
 }
