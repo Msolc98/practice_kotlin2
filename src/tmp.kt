8442ef0faf657920cbf0964e8aceb6a79c2cc779
Msolc98 <lillit0728@gmail.com>
2021-03-23 20:27:57 +0900

    2021.03.23.1st

--- src/tmp.kt
+++ src/tmp.kt
@@ -1,10 +1,16 @@
+import java.util.Scanner
+
 fun main()  {
     var test = tmp()
-    println(test.test())
+    var scanner:Scanner = Scanner(System.`in`)
+    test.test()
 }
 
 class tmp{
     fun test(){
-        var answer= 0
+
+        var array:List<Int> = listOf(1,2,3)
+        println(array)
+
     }
 }
---
+++ src/twozerotwoone_three/x만큼간격이있는n개의숫자.kt
@@ -0,0 +1,13 @@
+package twozerotwoone_three
+
+fun main(){
+
+
+}
+
+class Solution {
+    fun solution(x: Int, n: Int): LongArray {
+        var answer = longArrayOf()
+        return answer
+    }
+}
