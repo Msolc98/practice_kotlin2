a6f26322784dcfa12ce3313efa73a8bf9d5506e4
Msolc98 <lillit0728@gmail.com>
2021-03-23 23:27:14 +0900

    2021.03.23.2nd

--- src/twozerotwoone_three/x만큼간격이있는n개의숫자.kt
+++ src/twozerotwoone_three/x만큼간격이있는n개의숫자.kt
@@ -1,13 +1,19 @@
 package twozerotwoone_three
 
 fun main(){
-
+    var test = Solution23first()
+    println(test.solution(2,5))
+    print(Int.MIN_VALUE)
 
 }
 
-class Solution {
-    fun solution(x: Int, n: Int): LongArray {
-        var answer = longArrayOf()
+class Solution23first {
+
+    fun solution(x: Long, n: Int): MutableList<Long> {
+        var answer:MutableList<Long> = mutableListOf(x)
+        while (answer.size != n) {
+            answer.add(answer[(answer.size - 1)] + x)
+        }
         return answer
     }
 }
